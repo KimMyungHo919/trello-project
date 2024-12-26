d2399838f9db114b1ca5402566fdd81215af07ee
김명호 <kimmyungho@gimmyeonghoui-MacBookPro.local>
2024-12-26 13:11:29 +0900

    feat: BaseTimeEntity 추가

--- src/main/java/com/project/trello/domain/user/entity/User.java
+++ src/main/java/com/project/trello/domain/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.project.trello.domain.user.entity;
 
+import com.project.trello.domain.config.BaseTimeEntity;
 import com.project.trello.domain.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
-public class User {
+public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
